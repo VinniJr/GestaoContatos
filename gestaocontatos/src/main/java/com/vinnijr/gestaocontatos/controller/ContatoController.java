3790b1a91fab36c0d0b2cc8114d4ffccadc464e1
Vinicius Costa <vinni.jr@gmail.com>
2020-01-16 14:52:39 -0300

    Ajuste API

--- gestaocontatos/src/main/java/com/vinnijr/gestaocontatos/controller/ContatoController.java
+++ gestaocontatos/src/main/java/com/vinnijr/gestaocontatos/controller/ContatoController.java
@@ -1,29 +1,53 @@
 package com.vinnijr.gestaocontatos.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vinnijr.gestaocontatos.model.Contato;
-import com.vinnijr.gestaocontatos.repository.ContatoRepository;
+import com.vinnijr.gestaocontatos.service.ContatoService;
 
+@CrossOrigin("*")
 @RestController
-@RequestMapping("/")
+@RequestMapping("/gestao_contato")
 public class ContatoController {
 	
 	@Autowired
-	private ContatoRepository contatoRepository;
+	private ContatoService contatoService;
 
 	
-	@RequestMapping(method = RequestMethod.POST, value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(method = RequestMethod.POST, value = "/cadastrar", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Contato> cadastrarContato(@RequestBody Contato contato) {
-		return new ResponseEntity<Contato>(contatoRepository.save(contato), HttpStatus.CREATED);
+		return new ResponseEntity<Contato>(contatoService.cadastrar(contato), HttpStatus.CREATED);
 	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/listar", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Contato>> listar() {
+		List<Contato> contato = (List<Contato>) contatoService.buscarTodos();
+		return new ResponseEntity<List<Contato>>(contato, HttpStatus.OK);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, value = "/alterar", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Contato> alterar(@RequestBody Contato contato) {
+		return new ResponseEntity<Contato>(contatoService.cadastrar(contato), HttpStatus.OK);
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE, value = "/excluir/{id}")
+	public ResponseEntity<Contato> excluir(@PathVariable Long id) {
+		contatoService.excluir(id);
+		return new ResponseEntity<Contato>(HttpStatus.OK);
+	}
+
+
 
 
 }
--- gestaocontatos/src/main/java/com/vinnijr/gestaocontatos/service/ContatoService.java
+++ gestaocontatos/src/main/java/com/vinnijr/gestaocontatos/service/ContatoService.java
@@ -1,5 +1,37 @@
 package com.vinnijr.gestaocontatos.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vinnijr.gestaocontatos.model.Contato;
+import com.vinnijr.gestaocontatos.repository.ContatoRepository;
+
+@Service
 public class ContatoService {
+
+	@Autowired
+	private ContatoRepository contatoRepository;
+
+	public Contato cadastrar(Contato contato) {
+		return contatoRepository.save(contato);
+	}
+
+	public List<Contato> buscarTodos() {
+		return (List<Contato>) contatoRepository.findAll();
+	}
+	
+    public void excluir(Long id){
+    	contatoRepository.deleteById(id);
+    }
+    
+    public Optional<Contato> buscarPorId(Long id) {
+    	Optional<Contato> optionalContato = contatoRepository.findById(id);
+		return optionalContato;
+    }
+
+
 
 }
